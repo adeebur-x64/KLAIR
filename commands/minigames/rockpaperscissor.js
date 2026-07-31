48c794f20f44a029f9098eac6fdf24ab096b5a39
adeebur_x64.efi <domos.exe@gmail.com>
2026-07-31 20:57:24 +0530

    feat: rock paper scissors minigame

---
+++ commands/minigames/rockpaperscissor.js
@@ -0,0 +1,79 @@
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('rockpaperscissor').setDescription('Play Rock Paper Scissors with the bot!'),
+    async execute(interaction) {
+        // Building the options
+        const rock = new ButtonBuilder()
+            .setCustomId('rock')
+            .setLabel('Rock')
+            .setStyle(ButtonStyle.Success)
+            .setEmoji('🪨');
+
+        const paper = new ButtonBuilder()
+            .setCustomId('paper')
+            .setLabel('Paper')
+            .setStyle(ButtonStyle.Primary)
+            .setEmoji('📄');
+
+        const scissors = new ButtonBuilder()
+            .setCustomId('scissors')
+            .setLabel('Scissors')
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji('✂️');
+
+        const row = new ActionRowBuilder().addComponents(rock, paper, scissors);
+
+        // Sending the initial reply
+        const response = await interaction.reply({ content: 'Rock Paper Scissors Shoot! Pick your move below, I\'ll pick mine:', components: [row], withResponse: true });
+
+        // Bot choosing its own move
+        const options = ["rock", "paper", "scissors"];
+        const botChoice = options[Math.floor(Math.random() * options.length)];
+
+        const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+        try {
+            const selection = await response.resource.message.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+            if (selection.customId === 'rock') {
+
+                if (botChoice === 'paper') {
+                    await selection.update({ content: `You picked :rock: rock! I picked :page_facing_up: ${botChoice}\n**I Won!**`, components: [] });
+                } else if (botChoice === 'scissors') {
+                    await selection.update({ content: `You picked :rock: rock! I picked :scissors: ${botChoice}\n**You Won! Well, I wanted to win! :(**`, components: [] });
+                } else {
+                    await selection.update({ content: `You picked :rock: rock! I picked :rock: ${botChoice}\n**It's a Tie!**`, components: [] });
+                }
+
+            }
+
+            else if (selection.customId === 'paper') {
+
+                if (botChoice === 'rock') {
+                    await selection.update({ content: `You picked :page_facing_up: paper! I picked :rock: ${botChoice}\n**You Won! Well, I wanted to win! :(**`, components: [] });
+                } else if (botChoice === 'scissors') {
+                    await selection.update({ content: `You picked :page_facing_up: paper! I picked :scissors: ${botChoice}\n**I Won!**`, components: [] });
+                } else {
+                    await selection.update({ content: `You picked :page_facing_up: paper! I picked :page_facing_up: ${botChoice}\n**It's a Tie!**`, components: [] });
+                }
+
+            }
+
+            else if (selection.customId === 'scissors') {
+
+                if (botChoice === 'rock') {
+                    await selection.update({ content: `You picked :scissors: scissors! I picked :rock: ${botChoice}\n**I Won!**`, components: [] });
+                } else if (botChoice === 'paper') {
+                    await selection.update({ content: `You picked :scissors: scissors! I picked :page_facing_up: ${botChoice}\n**You Won! Well, I wanted to win! :(**`, components: [] });
+                } else {
+                    await selection.update({ content: `You picked :scissors: scissors! I picked :scissors: ${botChoice}\n**It's a Tie!**`, components: [] });
+                }
+
+            }
+
+        } catch {
+            await interaction.editReply({ content: 'You need to click the buttons to pick your choice! Run the command again to play!', components: [] });
+        }
+    },
+};
